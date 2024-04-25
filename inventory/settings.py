876d67e64b1e3f609857eed2b1767837d292787a
javohbro <salohiddinovj73@gmail.com>
2024-04-25 14:48:56 +0500

    initail commit

--- inventory/settings.py
+++ inventory/settings.py
@@ -122,8 +122,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
-MEDIA_URL = '/images/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
